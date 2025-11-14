a9f1cbe56d1c68a6cd2f5999d0e06e7f9be5ac6c
tanay-jagadeesh <tanayj2107@gmail.com>
2025-11-13 20:52:44 -0500

    converted input to title case for flexibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle
 import pandas
 
+score = 0
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -8,10 +9,21 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-game_on = True
+answer_state = screen.textinput(title = "Guess the State", prompt = "What's another's state's name?").title()
 
-while game_on:
-    answer_state = screen.textinput(title = "Guess the State", prompt = "What's another's state's name?")
+data = pandas.read_csv("50_states.csv")
+
+if data[data["state"] == answer_state]:
+    print(data[data["x"]], data[data["y"]])
+    print(f"{score}/50 states correct")
+
+
+
+
+
+
+
 
 
 turtle.mainloop()
+
