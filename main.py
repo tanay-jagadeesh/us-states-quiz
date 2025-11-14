0676d804f015ecdbc9624d03b6e2ce450a6ee0a1
tanay-jagadeesh <tanayj2107@gmail.com>
2025-11-13 21:30:49 -0500

    fixed some minor things

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import turtle
 import pandas
 
 game_on = True
-
+total = []
 score = 0
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -16,17 +17,19 @@ data = pandas.read_csv("50_states.csv")
 writer = turtle.Turtle()
 writer.hideturtle()
 writer.penup()
+
 while game_on:
     answer_state = screen.textinput(title = f"{score}/50 states correct Guess the State", prompt = "What's another's state's name?").title()
     correct_guess = data[data["state"] == answer_state]
 
-    if not correct_guess.empty: #if you found a matching row then do this
-        score += 1
-        writer.goto(correct_guess["x"].iloc[0], correct_guess["y"].iloc[0])
-        writer.write(correct_guess["state"].iloc[0])
-
-        if score == 50:
-            game_on = False
+    if answer_state not in total:
+        if not correct_guess.empty: #if you found a matching row then do this
+            score += 1
+            writer.goto(correct_guess["x"].iloc[0], correct_guess["y"].iloc[0])
+            writer.write(correct_guess["state"].iloc[0])
+            total.append(answer_state)
+            if score == 50:
+                game_on = False
 
 
 
