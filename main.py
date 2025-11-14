64f7d4d68da2313e1d294731256b69140dcb7a34
tanay-jagadeesh <tanayj2107@gmail.com>
2025-11-13 21:13:22 -0500

    reorganized code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import turtle
 import pandas
 
+game_on = True
+
 score = 0
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -9,19 +11,21 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-answer_state = screen.textinput(title = "Guess the State", prompt = "What's another's state's name?").title()
-
 data = pandas.read_csv("50_states.csv")
 
-correct_guess = data[data["state"] == answer_state]
+while game_on:
+    answer_state = screen.textinput(title = "Guess the State", prompt = "What's another's state's name?").title()
+    correct_guess = data[data["state"] == answer_state]
 
+    if not correct_guess.empty:
+        score += 1
+        print(f"{score}/50 states correct")
+        turtle.penup()
+        turtle.goto(correct_guess["x"].iloc[0], correct_guess["y"].iloc[0])
+        turtle.write(correct_guess["state"].iloc[0])
 
-if not correct_guess.empty:
-    score += 1
-    print(f"{score}/50 states correct")
-    turtle.penup()
-    turtle.goto(correct_guess["x"].iloc[0], correct_guess["y"].iloc[0])
-    turtle.write(correct_guess["state"].iloc[0])
+        if score == 50:
+            game_on = False
 
 
 
