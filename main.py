e2d19d2ba74cfd56b9e6f7b0bf6ae487d5ca5c9b
tanay-jagadeesh <tanayj2107@gmail.com>
2025-11-13 21:21:30 -0500

    fixed map movement

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 
+writer = turtle.Turtle()
+writer.hideturtle()
+writer.penup()
 while game_on:
     answer_state = screen.textinput(title = "Guess the State", prompt = "What's another's state's name?").title()
     correct_guess = data[data["state"] == answer_state]
@@ -20,9 +23,9 @@ while game_on:
     if not correct_guess.empty:
         score += 1
         print(f"{score}/50 states correct")
-        turtle.penup()
-        turtle.goto(correct_guess["x"].iloc[0], correct_guess["y"].iloc[0])
-        turtle.write(correct_guess["state"].iloc[0])
+        writer.penup()
+        writer.goto(correct_guess["x"].iloc[0], correct_guess["y"].iloc[0])
+        writer.write(correct_guess["state"].iloc[0])
 
         if score == 50:
             game_on = False
